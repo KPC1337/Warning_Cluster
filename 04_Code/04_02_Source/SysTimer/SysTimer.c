5b82570222fb86346e121168245e588f5458e12d
KPC1337 <kpc1337@gmail.com>
2021-06-16 22:03:56 +0530

    Divided system into modules. Integrated modules using .prm files
    ->Removed unnecessary files.
    ->Divided source file group into
        modules
    ->Made .h files and .prm files for each
        module
    ->Integrated Telltale module into
        SysTimer
    ->Fixed telltale drive write function by
        only writing when data in port is
        changed
    ->Created one second timer for
        SysTimer

--- 04_Code/04_02_Source/SysTimer/SysTimer.c
+++ 04_Code/04_02_Source/SysTimer/SysTimer.c
@@ -1,29 +1,30 @@
-#include "Common.h"
-#include "Telltale.h"
+#include"Telltale_Driver.prm"	
 
-void Task_1(void);
-void Task_2(void);
+void one_Second_Timer(void);
+void read_P1(void);
 void Task_3(void);
 void Event(void (*event_TBL[])(void));
 
 U1 u1_BUF_1MS;
 F1 St_Temp;
-U1 u1_Port;
 U1 u1_PrtSts;
+U1 u1_1Sec;
 
-void (*const MS10_TBL[])(void) = {&Task_1,&Task_2,&Task_3,NULL};
+
+void (*const MS10_TBL[])(void) = {one_Second_Timer,read_P1,Task_3,NULL};
 
 int main(void)
 { 
 	u1_BUF_1MS = (U1)OFF;
 	St_Temp.Bit.b00 = ON;
 	u1_PrtSts = (U1)0xFF;
+	u1_1Sec = (U1)0;
 	TT_Dr_Init();
 	
 	TMOD = 0x01;
 	IE   = 0x82;
-	TL0  = 0xD8;
-    TH0  = 0xF0;
+	TL0  = 0xF0;
+    TH0  = 0xD8;
 	TR0  = 1; 
 	
 	while(1)
@@ -41,27 +42,35 @@ void Timer0(void) interrupt 1
 {
   St_Temp.Bit.b00 = ON; 
   TF0 = 0; 
-  TL0  = 0x18;
-  TH0  = 0xFC;
+  TL0  = 0xF0;
+  TH0  = 0xD8;
   EA = 0;
 }
 
-void Task_1(void)
+void one_Second_Timer(void)
 {
+  	u1_1Sec = u1_1Sec+1;
+	if(u1_1Sec >= 100)
+	{
+	//P1_0 = ~P1_0;
+	u1_1Sec = 0;
+	}
+}
+
+void read_P1(void)
+{
+    U1 u1_Port;
 	u1_Port = P1;
 	if(u1_Port != u1_PrtSts){
 		TT_Dr_Write(P1);
 	}
 	u1_PrtSts = u1_Port;
-}
-
-void Task_2(void)
-{
-				
+					
 }
 
 void Task_3(void)
-{
+{     
+
 
 }
 
@@ -74,4 +83,5 @@ void Event(void (*const *MS10_TBL)(void))
 		MS10_TBL++;
 	}
 }
+
 
